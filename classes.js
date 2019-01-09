1700c3b21a3fd2a1f689838091f2438c91d566c9
vivian <vivian@vivians-MacBook-Pro.local>
2019-01-09 15:19:50 -0700

    completed

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " " + "Widget";
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -50,7 +60,24 @@
 */
 
 //Code Here
-
+class Manager{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " " + "Widget";
+  }
+  hire(employee){
+    return this.reports.push(employee)
+  }
+  fire(index){
+    return this.reports.splice(index,1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -76,7 +103,63 @@
 */
 
 //Code Here
-
+class ProgressiveManager{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = []
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + " " + "Widget";
+  }
+  hire(employee){
+    this.reports.push(employee) 
+    if(this.reports.length===0){
+      this.title = "Not a manager"
+    }
+    else if(this.reports.length>=1 && this.reports.length<=3){
+      this.title = "Barely Manager"
+    }
+    else if(this.reports.length>=4 && this.reports.length<=10){
+      this.title = "Mostly Manager"
+    }
+    else if(this.reports.length>=11 && this.reports.length<=50){
+      this.title = "Manager"
+    }
+    else if(this.reports.length>=51 && this.reports.length<=100){
+      this.title = "Manager Plus"
+    }
+    else if(this.reports.length>=101){
+      this.title = "Bestest Manager"
+    }
+  }
+  fire(index){
+    this.reports.splice(index,1);
+    this.bonus += 100;
+    if(this.reports.length===0){
+      this.title = "Not a manager"
+    }
+    else if(this.reports.length>=1 && this.reports.length<=3){
+      this.title = "Barely Manager"
+    }
+    else if(this.reports.length>=4 && this.reports.length<=10){
+      this.title = "Mostly Manager"
+    }
+    else if(this.reports.length>=11 && this.reports.length<=50){
+      this.title = "Manager"
+    }
+    else if(this.reports.length>=51 && this.reports.length<=100){
+      this.title = "Manager Plus"
+    }
+    else if(this.reports.length>=101){
+      this.title = "Bestest Manager"
+    }
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -103,5 +186,26 @@
 */
 
 //Code Here
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count+=num
+    this.wear_and_tear_count = this.widgets_made_count/50
+  }
+  fixMachine(){
+    this.needs_reboot = true
+  }
+  reboot(){
+    return () => {
+      if(this.needs_reboot===true){
+        this.wear_and_tear_count-=10;
+        this.needs_reboot = false;
+      }
+    }
+  }
+}
 
